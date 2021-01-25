19d1eed692cf6486921b9d40ebd9a9457854994e
Danila <danila.litskevich@gmail.com>
2021-01-25 20:12:17 +0300

    mcts added

---
+++ app/ai/mcts.js
@@ -0,0 +1,175 @@
+export class MonteCarloTreeSearch {
+  init () {
+    return {
+      scoreForWin: 10
+    }
+  }
+
+  findNextmove (board, player) {
+    const opponent = 3 - player
+    const tree = new Tree()
+    const rootNode = tree.root
+    rootNode.state.board = board
+    rootNode.state.player = opponent
+
+    const startTime = Date.now()
+    while ((Date.now() - startTime) < 1000) {
+      const promisingNode = this.selectPromisingNode(rootNode)
+      if (promisingNode.state.board.checkWinner() === false) {
+        this.expandNode(promisingNode)
+      }
+      let nodeToExplore = promisingNode
+      if (promisingNode.childs.length > 0) {
+        nodeToExplore = promisingNode.randomChild()
+      }
+      const playoutResult = this.simulateRandomPlayout(nodeToExplore, opponent)
+      this.backPropogation(nodeToExplore, playoutResult)
+    }
+
+    const winnerNode = rootNode.childs.reduce((acc, cur) => cur.state.visits > acc.state.visits ? cur : acc)
+    console.log(rootNode)
+    // tree.root = winnerNode
+    return winnerNode.state.board
+  }
+
+  selectPromisingNode (rootNode) {
+    let node = rootNode
+    while (node.childs.length !== 0) {
+      node = UCT.findBestChild(node)
+    }
+    return node
+  }
+
+  expandNode (node) {
+    const possibleStates = node.state.getPossibleStates()
+    possibleStates.forEach((state) => {
+      const newNode = new Node(state, node)
+      newNode.state.player = 3 - node.state.player
+      node.childs.push(newNode)
+    })
+  }
+
+  simulateRandomPlayout (nodeToExplore, opponent) {
+    const tempNode = new Node(nodeToExplore.state, nodeToExplore.parent)
+    const tempState = tempNode.state
+    let winner = tempState.board.checkWinner()
+    // if (winner) {
+    //   console.log(winner, opponent)
+    //   console.log(tempState)
+    //   console.log(tempNode)
+    // }
+    if (winner === opponent) {
+      console.log(123, JSON.parse(JSON.stringify(nodeToExplore.state)))
+      console.log(123, JSON.parse(JSON.stringify(nodeToExplore.parent.state)))
+      console.log(JSON.parse(JSON.stringify(tempNode.state)))
+
+      tempNode.parent.state.score = Number.MIN_SAFE_INTEGER
+      return winner
+    }
+    tempNode.parent = null
+    while (winner === false) {
+      tempState.togglePlayer()
+      tempState.randomMove()
+      winner = tempState.board.checkWinner()
+    }
+    return winner
+  }
+
+  backPropogation (nodeToExplore, winner) {
+    let tempNode = nodeToExplore
+    while (tempNode !== null) {
+      tempNode.state.visits++
+      if (tempNode.state.player === winner) {
+        tempNode.state.addScore(1)
+      }
+      tempNode = tempNode.parent
+    }
+  }
+}
+
+const UCT = {
+  uctValue (totalVisit, nodeWinScore, nodeVisit) {
+    if (nodeVisit === 0) {
+      return Number.MAX_SAFE_INTEGER
+    }
+    return (nodeWinScore / nodeVisit) + 1.41 * Math.sqrt(Math.log(totalVisit) / nodeVisit)
+  },
+
+  findBestChild (node) {
+    const parentVisits = node.state.visits
+    const values = node.childs.map((child) => UCT.uctValue(parentVisits, child.state.score, child.state.visits))
+    const id = values.reduce((iMax, cur, i, arr) => cur > arr[iMax] ? i : iMax, 0)
+    return node.childs[id]
+  }
+}
+
+class Tree {
+  constructor (root) {
+    this.root = new Node()
+  }
+}
+
+class Node {
+  constructor (state = new State(), parent = null, childs = []) {
+    if (arguments.length === 1) {
+      this.state = new State(state)
+      this.parent = parent
+      this.childs = childs
+    } else if (arguments.length === 2) {
+      this.state = new State(state)
+      this.parent = parent
+      this.childs = childs
+    } else {
+      this.state = state
+      this.parent = parent
+      this.childs = childs
+    }
+  }
+
+  randomChild () {
+    return this.childs[Math.floor(Math.random() * this.childs.length)]
+  }
+}
+
+class State {
+  constructor (state) {
+    if (arguments.length === 1) {
+      this.visits = state.visits
+      this.score = state.score
+      this.board = state.board.makeCopy()
+      this.player = state.player
+    } else {
+      this.visits = 0
+      this.score = 10
+      this.board = null
+      this.player = null
+    }
+  }
+
+  togglePlayer () {
+    this.player = 3 - this.player
+  }
+
+  addScore (score) {
+    if (this.score !== Number.MIN_SAFE_INTEGER) {
+      this.score += score
+    }
+  }
+
+  getPossibleStates () {
+    const availableMoves = this.board.getAvailableMoves()
+    return availableMoves.map((move) => {
+      const newState = new State()
+      newState.board = this.board.makeCopy()
+      newState.player = 3 - this.player
+      newState.board.performMove(move, newState.player)
+      return newState
+    })
+  }
+
+  randomMove () {
+    const availableMoves = this.board.getAvailableMoves()
+    const randId = Math.floor(Math.random() * availableMoves.length)
+    this.board.performMove(availableMoves[randId], this.player)
+  }
+}
--- app/uttt_store.js
+++ app/uttt_store.js
@@ -1,108 +1,41 @@
 /* eslint-disable space-before-function-paren */
-function reset() {
-  const size = [0, 1, 2]
-  return {
-    board: size.map(row => ({
-      id: row,
-      cols: size.map(col => ({
-        id: col,
-        board: size.map(row => ({ id: row, cols: size.map(col => ({ id: col, value: 0 })) })),
-        value: 0,
-        step: 0,
-        finished: false,
-        available: 1
-      }))
-    })),
-    step: 0,
-    finished: false
-  }
-}
-function checkFinished(board, ultstep, step) {
-  if (step < 2 && step !== -1) {
-    return false
-  }
-  const player = 1 + ultstep % 2
-  for (let row = 0; row < 3; row++) {
-    if (board[row].cols.reduce((acc, cur) => (acc && (cur.value === player)), true)) {
-      return player
-    }
-    if (board.reduce((acc, cur) => (acc && (cur.cols[row].value === player)), true)) {
-      return player
-    }
-  }
-  if (board.reduce((acc, cur, ind) => (acc && (cur.cols[ind].value === player)), true)) {
-    return player
-  }
-  if (board.reduce((acc, cur, ind) => (acc && (cur.cols[2 - ind].value === player)), true)) {
-    return player
-  }
-  if (step === 8) {
-    return 3
-  }
-  // ultimative case
-  if (step === -1) {
-    // const numAvailable = 0
-    for (let r = 0; r < 3; r++) {
-      for (let c = 0; c < 3; c++) {
-        if (board[r].cols[c].available !== false) {
-          return false
-        }
-      }
-    }
-    return 3
-  }
-  return false
-}
+import { UTTTBoard } from './ai/uttt.js'
+import { MonteCarloTreeSearch } from './ai/mcts.js'
 
-export class UTTT {
+export class UTTT extends UTTTBoard {
   init() {
-    return reset()
+    return this.reset()
   }
 
-  onStepcell({ col, row, value, subrow, subcol }, { board, step, finished }) {
-    const subboard = board[subrow].cols[subcol]
-    // console.log(subboard)
-    if (!value && !finished && (subboard.available === 1) && !subboard.value && !subboard.finished) {
-      subboard.board[row].cols[col].value = 1 + step % 2
-      subboard.finished = checkFinished(subboard.board, step, subboard.step)
-      subboard.step = 1 + subboard.step
-      board[subrow].cols[subcol] = subboard
-      if (subboard.finished) {
-        subboard.value = subboard.finished % 3
-        subboard.available = false
-        board[subrow].cols[subcol] = subboard
-        finished = checkFinished(board, step, -1)
+  onStepcell(cell, game) {
+    if (this.toggledAI) {
+      if (this.toggledAI !== 1 + game.step % 2) {
+        const nextState = this.makeMove(cell, game)
+        return { ...nextState, '...': Promise.resolve(this.airequest(nextState)) }
       }
-      let avail = 1
-      // console.log(subrow)
-      // console.log(subcol)
-      if (board[row].cols[col].available !== false || finished) {
-        avail = 0
-      }
-      for (let r = 0; r < 3; r++) {
-        for (let c = 0; c < 3; c++) {
-          if (board[r].cols[c].available !== false) {
-            board[r].cols[c].available = avail
-          }
-        }
-      }
-      if (avail === 0 && !finished) {
-        board[row].cols[col].available = 1
-      }
+    }
+    return this.makeMove(cell, game)
+  }
 
-      return { board: [...board], step: step + 1, finished: finished }
+  onAirequest() {
+    return this.airequest(this)
+  }
+
+  onToggleai({ state }) {
+    this.toggledAI = state
+    if (state === 1 + this.step % 2) {
+      return this.airequest(this)
     }
   }
-
-  // onStepSub({ col, row, value }, { board, step, finished }) {
-  //   if (!value && !finished) {
-  //     board[row].cols[col].value = 1 + step % 2
-  //     finished = checkFinished(board, step)
-  //     return { board: [...board], step: step + 1, finished: finished }
-  //   }
-  // }
 
   onReset(_, { board }) {
-    return reset()
+    return this.reset()
+  }
+
+  airequest (state) {
+    if (!this.montecarlo) {
+      this.montecarlo = new MonteCarloTreeSearch()
+    }
+    return this.montecarlo.findNextmove(this.makeCopy(), 1 + state.step % 2)
   }
 };
